7eb9f7a6ce2e82d0c7546d98197f6508268bec35
Lionel FARGEAS <lfargeas@e1r8p6.42.fr>
2016-01-06 01:18:57 +0100

    get_next_line.c

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,21 @@
 /*   By: lfargeas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 01:14:01 by lfargeas          #+#    #+#             */
-/*   Updated: 2015/12/09 01:25:07 by lfargeas         ###   ########.fr       */
+/*   Updated: 2016/01/06 00:28:29 by lfargeas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFF_SIZE 32
+#define BUFF_SIZE 100
+
+typedef struct		s_data
+{
+	char *str;
+	int fd_reading;
+}					t_data;
+
 int get_next_line(int const fd, char ** line);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lfargeas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 03:19:19 by lfargeas          #+#    #+#             */
-/*   Updated: 2015/12/19 12:55:01 by lfargeas         ###   ########.fr       */
+/*   Updated: 2016/01/06 01:16:29 by lfargeas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,22 @@
 #include "libft/libft.h"
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main ()
 {
-	char **line;
+	int nbr;
+	char *line;
+	line = NULL;
 	int fd = open("lol.getnextline", O_RDWR	);
-	get_next_line(fd, line);
+	while ((nbr = get_next_line(fd, &line)))
+	{
+		ft_putnbr(nbr);
+		ft_putendl(line);
+	}
+	ft_putnbr(nbr);
+	ft_putstr(line);
+	close(fd);
 	return (0);
 }
